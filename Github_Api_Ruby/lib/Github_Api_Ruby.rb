35850aa6bd502ac9304457621871c11af220612f
jacob <jabrah29@uic.edu>
2017-07-19 01:51:55 -0400

    check

--- Github_Api_Ruby/lib/Github_Api_Ruby.rb
+++ Github_Api_Ruby/lib/Github_Api_Ruby.rb
@@ -1,5 +1,10 @@
 require "Github_Api_Ruby/version"
 
 module GithubApiRuby
-  # Your code goes here...
+    require 'lib/http/repo_http_handler'
+    require 'lib/http/user_http_handler'
+    require 'lib/http/uri_builder'
+    require 'lib/http/api_options'
+    require 'lib/http/http_handler'
+
 end
